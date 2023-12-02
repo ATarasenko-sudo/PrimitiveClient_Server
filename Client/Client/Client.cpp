3bcc93eb67449675ad21732281d27a3317cb4724
ATarasenko-sudo <andrey.tarasenko.00@list.ru>
2023-12-02 16:54:49 +0300

    upload to programms

---
+++ Client/Client/Client.cpp
@@ -0,0 +1,117 @@
+﻿
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#endif
+
+#include <iostream>
+#include <cstring>
+
+const char* SERVER_IP = "127.0.0.1";
+const int PORT = 12345;
+const int BUFFER_SIZE = 1024;
+
+#ifdef _WIN32
+// Преобразование строки в формат PCWSTR
+std::wstring ConvertToWideString(const char* narrowString) {
+    int length = MultiByteToWideChar(CP_ACP, 0, narrowString, -1, NULL, 0);
+    wchar_t* wideString = new wchar_t[length];
+    MultiByteToWideChar(CP_ACP, 0, narrowString, -1, wideString, length);
+    std::wstring result(wideString);
+    delete[] wideString;
+    return result;
+}
+#endif
+
+int main() {
+#ifdef _WIN32
+    // Инициализация библиотеки сокетов для Windows
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Error while initializing the socket library" << std::endl;
+        return -1;
+    }
+#endif
+
+    // Создание сокета
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "Error preparing socket" << std::endl;
+#ifdef _WIN32
+        closesocket(clientSocket);
+#else
+        close(clientSocket);
+#endif
+        return -1;
+    }
+
+    // Заполнение информации о сервере
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
+
+#ifdef _WIN32
+    // Для Windows используем InetPton
+    std::wstring wideServerIP = ConvertToWideString(SERVER_IP);
+    if (InetPton(AF_INET, wideServerIP.c_str(), &(serverAddress.sin_addr)) <= 0) {
+        std::cerr << "Error while resolving IP address" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return -1;
+    }
+#else
+    // Для других платформ используем inet_pton
+    if (inet_pton(AF_INET, SERVER_IP, &(serverAddress.sin_addr)) <= 0) {
+        std::cerr << "Error while resolving IP address" << std::endl;
+        close(clientSocket);
+        return -1;
+    }
+#endif
+
+    // Подключение к серверу
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Error connecting to server" << std::endl;
+#ifdef _WIN32
+        closesocket(clientSocket);
+#else
+        close(clientSocket);
+#endif
+        return -1;
+    }
+
+    std::cout << "Connected to server" << std::endl;
+
+    char buffer[BUFFER_SIZE];
+    while (true) {
+        std::cout << "Send to server : ";
+        std::cin.getline(buffer, BUFFER_SIZE);
+
+        // Отправка данных на сервер
+        send(clientSocket, buffer, strlen(buffer), 0);
+
+        if (strcmp(buffer, "exit") == 0) {
+            break;
+        }
+
+        // Получение ответа от сервера
+        memset(buffer, 0, BUFFER_SIZE);
+        recv(clientSocket, buffer, BUFFER_SIZE, 0);
+        std::cout << "Received from server:" << buffer << std::endl;
+    }
+
+    // Закрытие сокета
+#ifdef _WIN32
+    closesocket(clientSocket);
+    WSACleanup();
+#else
+    close(clientSocket);
+#endif
+
+    return 0;
+}
---
+++ Server/Server/Server.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <cstring>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#endif
+
+const int PORT = 12345;
+const int BUFFER_SIZE = 1024;
+
+int main() {
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Error while initializing the socket library" << std::endl;
+        return -1;
+    }
+#endif
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Error creating socket" << std::endl;
+#ifdef _WIN32
+        closesocket(serverSocket);
+#else
+        close(serverSocket);
+#endif
+        return -1;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Error when binding a socket to an address and port" << std::endl;
+#ifdef _WIN32
+        closesocket(serverSocket);
+#else
+        close(serverSocket);
+#endif
+        return -1;
+    }
+
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "Error when putting the socket into listening mode" << std::endl;
+#ifdef _WIN32
+        closesocket(serverSocket);
+#else
+        close(serverSocket);
+#endif
+        return -1;
+    }
+
+    std::cout << "The server is waiting for connection..." << std::endl;
+
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+    if (clientSocket == -1) {
+        std::cerr << "Error on your client connection" << std::endl;
+#ifdef _WIN32
+        closesocket(serverSocket);
+#else
+        close(serverSocket);
+#endif
+        return -1;
+    }
+
+    std::cout << "Client Connected" << std::endl;
+
+    char buffer[BUFFER_SIZE];
+    while (true) {
+        memset(buffer, 0, BUFFER_SIZE);
+
+        if (recv(clientSocket, buffer, BUFFER_SIZE, 0) == -1) {
+            std::cerr << "Error receiving data from client" << std::endl;
+            break;
+        }
+
+        std::cout << "Received from server : " << buffer << std::endl;
+
+        std::cout << "Send response to client:";
+        std::cin.getline(buffer, BUFFER_SIZE);
+        send(clientSocket, buffer, strlen(buffer), 0);
+    }
+
+#ifdef _WIN32
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+#else
+    close(clientSocket);
+    close(serverSocket);
+#endif
+
+#ifdef _WIN32
+    WSACleanup();
+#endif
+
+    return 0;
+}
